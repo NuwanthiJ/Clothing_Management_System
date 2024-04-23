085899fd789e41026e9faf13c81097c452944356
Maheesha Erangi Gamage <160692822+Maheeshaerangi@users.noreply.github.com>
2024-04-23 19:22:20 +0530

    Server-item details add & sample login add

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dbCon from './utils/db.util.js'
 import cors from 'cors'
 import feedbackRouter from './routes/feedback.route.js'
+import itemRoutes from './routes/item.js'
+import itemdetailsRoutes from './routes/itemdetails.js'
 
 const app = express()
 dbCon()
@@ -10,6 +12,9 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/feedback', feedbackRouter)
+app.use('/items', itemRoutes);
+app.use('/itemdetails', itemdetailsRoutes);
+
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -3,12 +3,19 @@ import React from 'react'
 
 
 const Login = () => {
+  // This is a temporary login function
+  const handleLogin = () => {
+    localStorage.setItem("userId", "user2");
+    window.location.reload();
+  };
+  //
   return (
     <div>  
       <h1>login page</h1>
-      <button className="loginbtn">LOGIN</button>
+      <button className="loginbtn"onClick={handleLogin}>LOGIN
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Login
