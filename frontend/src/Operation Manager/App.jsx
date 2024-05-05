2778ea43f709cb83cfe5d0a66eb8b00dccd7b072
IT22540430 <IT22540430@my.sliit.lk>
2024-05-05 18:23:17 +0530

    firstcommit

---
+++ frontend/src/Operation Manager/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Login'
+import {BrowserRouter, Routes,Rout, BrowserRouter} from 'react-router-dom'
+
+function App(){
+    const {count,setCount} =UseState(0)
+
+    return(
+        <BrowserRouter>
+         <Routes>
+             
+
+         </Routes>
+        </BrowserRouter>
+       
+    )
+}
+
+export default App
